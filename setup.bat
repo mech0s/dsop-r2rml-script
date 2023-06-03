f51c34df1d0ead64f9bf05c8f775c9cd213e4fe2
donald <donald@mechos.net>
2023-06-03 13:33:58 +0100

    intial commit

---
+++ setup.bat
@@ -0,0 +1,3 @@
+REM set PATH=D:\eclipse-modeling-2020-09-epsilon-xtext\jdk-11.0.6\bin;%PATH%
+REM set JAVA_HOME=D:\eclipse-modeling-2020-09-epsilon-xtext\jdk-11.0.6
+doskey r2rml=java -jar D:\work\r2rml\r2rml.jar $*
